7c134cf7a2167021adc940b36efb65625e7ee517
dinny-xu <17501606708@163.com>
2021-08-04 14:06:48 +0800

    Initial commit

---
+++ src/main/java/com/example/security/SpringSecurityApplication.java
@@ -0,0 +1,30 @@
+package com.example.security;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@SpringBootApplication
+@Slf4j
+public class SpringSecurityApplication {
+
+    public static void main(String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext application = SpringApplication.run(SpringSecurityApplication.class, args);
+        Environment env = application.getEnvironment();
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        String port = env.getProperty("server.port");
+        String path = env.getProperty("server.servlet.context-path");
+        log.info("\n----------------------------------------------------------\n\t" +
+                "Application Netty is running! Access URLs:\n\t" +
+                "Local: \t\thttp://localhost:" + port + path + "\n\t" +
+                "External: \thttp://" + ip + ":" + port + path + "\n\t" +
+                "swagger-ui: \thttp://" + ip + ":" + port + path + "swagger-ui.html\n\t" +
+                "Doc: \t\thttp://" + ip + ":" + port + path + "doc.html\n" +
+                "----------------------------------------------------------");
+    }
+}
